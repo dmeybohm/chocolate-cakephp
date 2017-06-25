747908d4267d198e6b50f544de03eb9e2697d40a
David Meybohm <dmeybohm@gmail.com>
2017-06-25 15:43:26 -0400

    Initial commit

---
+++ src/com/daveme/intellij/chocolatecakephp/TemplateGotoDeclarationHandler.java
@@ -0,0 +1,23 @@
+package com.daveme.intellij.chocolatecakephp;
+
+import com.intellij.codeInsight.navigation.actions.GotoDeclarationHandler;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.Nullable;
+
+public class TemplateGotoDeclarationHandler implements GotoDeclarationHandler {
+
+    @Nullable
+    @Override
+    public PsiElement[] getGotoDeclarationTargets(@Nullable PsiElement psiElement, int i, Editor editor) {
+        return new PsiElement[0];
+    }
+
+    @Nullable
+    @Override
+    public String getActionText(DataContext dataContext) {
+        return null;
+    }
+}
+
