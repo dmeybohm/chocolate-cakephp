edc4701cf863f3ec3e7edf9dbfe554cda390d18f
David Meybohm <dmeybohm@gmail.com>
2017-09-10 11:01:58 -0500

    Move completion to separate class and support components

--- src/com/daveme/intellij/chocolateCakePHP/completion/ControllerCompletionProvider.java
+++ src/com/daveme/intellij/chocolateCakePHP/completion/ControllerCompletionProvider.java
@@ -1,25 +1,20 @@
 package com.daveme.intellij.chocolateCakePHP.completion;
 
-import com.daveme.intellij.chocolateCakePHP.util.StringUtil;
+import com.daveme.intellij.chocolateCakePHP.util.*;
 import com.intellij.codeInsight.completion.*;
-import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.PhpIndex;
-import com.jetbrains.php.completion.PhpVariantsUtil;
-import com.jetbrains.php.completion.UsageContext;
 import com.jetbrains.php.lang.psi.elements.FieldReference;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.PhpExpression;
-import com.jetbrains.php.lang.psi.elements.PhpModifier;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
-import java.util.List;
 
 public class ControllerCompletionProvider extends CompletionContributor {
 
@@ -57,24 +52,19 @@ public class ControllerCompletionProvider extends CompletionContributor {
                 if (classReference == null) {
                     return;
                 }
-                String className = classReference.getName();
+                String fieldName = classReference.getName();
                 PhpIndex phpIndex = PhpIndex.getInstance(psiElement.getProject());
                 String controllerClassName = controllerName + "Controller";
                 Collection<PhpClass> controllerClasses = phpIndex.getClassesByFQN(controllerClassName);
                 if (controllerClasses.size() == 0) {
                     return;
                 }
-                Collection<PhpClass> classes = phpIndex.getClassesByFQN(className);
-                UsageContext usageContext = new UsageContext(PhpModifier.State.DYNAMIC);
-                usageContext.setTargetObjectClass(controllerClasses.iterator().next());
-                for (PhpClass klass: classes) {
-                    try {
-                        List<LookupElement> lookupItems = PhpVariantsUtil.getLookupItems(klass.getMethods(), false, usageContext);
-                        completionResultSet.addAllElements(lookupItems);
-                    } catch (Exception e) {
 
-                    }
-                }
+                Collection<PhpClass> modelClasses = phpIndex.getClassesByFQN(fieldName);
+                PhpClass controllerClass = controllerClasses.iterator().next();
+                CakeCompletionUtil.complete(modelClasses, controllerClass, completionResultSet);
+                Collection<PhpClass> componentClasses = phpIndex.getClassesByFQN(fieldName + "Component");
+                CakeCompletionUtil.complete(componentClasses, controllerClass, completionResultSet);
             }
         });
     }
---
+++ src/com/daveme/intellij/chocolateCakePHP/util/CakeCompletionUtil.java
@@ -0,0 +1,41 @@
+package com.daveme.intellij.chocolateCakePHP.util;
+
+
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.jetbrains.php.completion.PhpVariantsUtil;
+import com.jetbrains.php.completion.UsageContext;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.jetbrains.php.lang.psi.elements.PhpModifier;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CakeCompletionUtil {
+
+    public static void complete(
+            @NotNull Collection<PhpClass> classes,
+            @NotNull PhpClass fromClass,
+            @NotNull CompletionResultSet completionResultSet) {
+        if (classes.size() == 0) {
+            return;
+        }
+        UsageContext usageContext = new UsageContext(PhpModifier.State.DYNAMIC);
+        usageContext.setTargetObjectClass(fromClass);
+        for (PhpClass klass: classes) {
+            try {
+                List<LookupElement> lookupItems = PhpVariantsUtil.getLookupItems(klass.getMethods(), false, usageContext);
+                completionResultSet.addAllElements(lookupItems);
+            } catch (Exception e) {
+
+            }
+            try {
+                List<LookupElement> lookupElements = PhpVariantsUtil.getLookupItems(klass.getFields(), false, usageContext);
+                completionResultSet.addAllElements(lookupElements);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+}
