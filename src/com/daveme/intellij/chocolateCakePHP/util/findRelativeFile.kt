b48916f083c9dc082cca8f37c78c2a04d09ca671
David Meybohm <dmeybohm@gmail.com>
2018-09-01 20:19:23 -0400

    Rewrite findRelativeFile to be a little more readable

--- src/com/daveme/intellij/chocolateCakePHP/util/findRelativeFile.kt
+++ src/com/daveme/intellij/chocolateCakePHP/util/findRelativeFile.kt
@@ -1,10 +1,13 @@
 package com.daveme.intellij.chocolateCakePHP.util
 
+import com.intellij.openapi.vfs.VfsUtil.findRelativeFile as vfsUtilFindRelativeFile
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
 
 fun findRelativeFile(dir: PsiDirectory?, childPath: String): VirtualFile? {
-    return if (dir == null) {
-        null
-    } else com.intellij.openapi.vfs.VfsUtil.findRelativeFile(dir.virtualFile, *childPath.split("/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
+    if (dir == null) {
+        return null
+    }
+    val pathComponents = childPath.split("/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    return vfsUtilFindRelativeFile(dir.virtualFile, *pathComponents)
 }
