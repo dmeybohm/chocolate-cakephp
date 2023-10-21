260b16334621b391f93049f6351fe89a779e9223
David Meybohm <dmeybohm@gmail.com>
2023-10-20 23:40:52 -0400

    Create action
    Create action so that undoing creating the file can work.

---
+++ src/main/kotlin/com/daveme/chocolateCakePHP/controller/CreateViewFileAction.kt
@@ -0,0 +1,79 @@
+package com.daveme.chocolateCakePHP.controller
+
+import com.daveme.chocolateCakePHP.CakeIcons
+import com.intellij.ide.fileTemplates.FileTemplateManager
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
+import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.PsiManager
+import com.intellij.psi.codeStyle.CodeStyleManager
+
+class CreateViewFileAction(
+    val destinationPath: String = "",
+    val useCustomPath: Boolean = false,
+) : AnAction(CakeIcons.LOGO) {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.getData(CommonDataKeys.PROJECT) ?: return
+
+        // Create file
+        WriteCommandAction.runWriteCommandAction(
+            project,
+            "Create View File",
+            null,
+            object : Runnable {
+                override fun run() {
+                    val template = FileTemplateManager.getInstance(project)
+                        .getInternalTemplate("CakePHP View File.php")
+                    val text = template.getText()
+
+                    val baseDir = project.guessProjectDir() ?: return
+
+                    // TODO: also create directories that don't exist along path here.
+                    val lastSlash = destinationPath.lastIndexOf('/')
+                    val parentDir = destinationPath.substring(0, lastSlash)
+                    val filename = destinationPath.substring(lastSlash + 1)
+                    val parentDirVirtualFile = baseDir.findFileByRelativePath(parentDir)
+                    val parentDirPsiFile = if (parentDirVirtualFile != null && parentDirVirtualFile.isDirectory) {
+                        PsiManager.getInstance(project).findDirectory(parentDirVirtualFile)
+                    } else {
+                        null
+                    } ?: return
+
+                    val psiFile = PsiFileFactory.getInstance(project)
+                        .createFileFromText(
+                            filename,
+                            FileTypeManager.getInstance().getFileTypeByFileName(filename),
+                            text
+                        )
+                    CodeStyleManager.getInstance(project).reformat(psiFile)
+
+                    val result = parentDirPsiFile.add(psiFile)
+                    if (result is PsiFile) {
+                        OpenFileDescriptor(project, result.virtualFile).navigate(true)
+                    }
+                }
+            }
+        )
+    }
+
+    override fun update(event: AnActionEvent) {
+        val project = event.getData(CommonDataKeys.PROJECT)
+        if (project == null || destinationPath == "") {
+            event.presentation.isEnabled = false
+            return
+        }
+        event.presentation.setEnabledAndVisible(true)
+        event.presentation.text = if (useCustomPath)
+            "Create Custom View File"
+        else
+            "Create Default View File"
+    }
+
+}
--- src/main/kotlin/com/daveme/chocolateCakePHP/controller/NavigateToCreatedFile.kt
+++ src/main/kotlin/com/daveme/chocolateCakePHP/controller/NavigateToCreatedFile.kt
@@ -1,90 +1,38 @@
 package com.daveme.chocolateCakePHP.controller
 
-import com.daveme.chocolateCakePHP.Settings
-import com.daveme.chocolateCakePHP.topSourceDirectoryFromFile
-import com.daveme.chocolateCakePHP.virtualFileToPsiFile
 import com.intellij.codeInsight.daemon.GutterIconNavigationHandler
-import com.intellij.ide.fileTemplates.FileTemplate
-import com.intellij.ide.fileTemplates.FileTemplateManager
-import com.intellij.ide.impl.ProjectUtil
-import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.OpenFileDescriptor
-import com.intellij.openapi.fileTypes.FileTypeManager
-import com.intellij.openapi.project.guessProjectDir
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiFileFactory
-import com.intellij.psi.PsiManager
-import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.ui.awt.RelativePoint
 import java.awt.event.MouseEvent
-import javax.swing.SwingUtilities
-
 
 class NavigateToCreatedFile(val destinationPath: String) : GutterIconNavigationHandler<PsiElement> {
 
-    override fun navigate(e: MouseEvent, elt: PsiElement?) {
-        val element = elt ?: return
-        val project = element.project
-
-        val resultPath = destinationPath
-        val popup = JBPopupFactory.getInstance()
-            .createConfirmation("Create View File", "Create view file", "Cancel",
-                {
-                    val template: FileTemplate = FileTemplateManager.getInstance(project)
-                        .getInternalTemplate("CakePHP View File.php")
-                    println("Internal template: ${template}")
-//                        .getCodeTemplate("PHP File.php")
-                    val text = template.getText()
-
-                    // Create file
-                    WriteCommandAction.runWriteCommandAction(project) {
-                        val baseDir = project.guessProjectDir() ?: return@runWriteCommandAction
-
-                        // TODO: also create directories that don't exist along path here.
-                        val lastSlash = resultPath.lastIndexOf('/')
-                        val parentDir = resultPath.substring(0, lastSlash)
-                        val filename = resultPath.substring(lastSlash + 1)
-                        val parentDirVirtualFile = baseDir.findFileByRelativePath(parentDir)
-                        val parentDirPsiFile = if (parentDirVirtualFile != null && parentDirVirtualFile.isDirectory) {
-                            PsiManager.getInstance(project).findDirectory(parentDirVirtualFile)
-                        } else {
-                            null
-                        } ?: return@runWriteCommandAction
-
-                        val psiFile = PsiFileFactory.getInstance(project)
-                            .createFileFromText(
-                                filename,
-                                FileTypeManager.getInstance().getFileTypeByFileName(filename),
-                                text
-                            )
-                        CodeStyleManager.getInstance(project).reformat(psiFile)
-
-                        val result = parentDirPsiFile.add(psiFile)
-                        if (result is PsiFile) {
-                            OpenFileDescriptor(project, result.virtualFile).navigate(true)
-//                            if (editor != null) {
-//                                FileEditorManager.getInstance(project).scrollToCaret(editor)
-//                            }
-
-                        }
-                    }
-                },
-                0
+    class NavigateToCreateFileActionGroup(val destinationPath: String) : ActionGroup() {
+        override fun getChildren(e: AnActionEvent?): Array<AnAction> {
+            return arrayOf(
+                CreateViewFileAction(destinationPath, useCustomPath = false),
+                CreateViewFileAction(destinationPath, useCustomPath = true)
             )
+        }
+    }
 
-//        val step = BaseListPopupStep("Create view file", listOf("Create view file"))
-//        val popup = JBPopupFactory.getInstance()
-//            .createListPopup(step)
-//            .addListSelectionListener(object {
-//                k
-//            })
+    override fun navigate(e: MouseEvent, elt: PsiElement?) {
+        val context = DataManager.getInstance().getDataContext(e.component)
+        val popup = JBPopupFactory.getInstance()
+            .createActionGroupPopup(
+                "Create View File",
+                NavigateToCreateFileActionGroup(destinationPath),
+                context,
+                JBPopupFactory.ActionSelectionAid.NUMBERING,
+                true,
+            )
 
         popup.show(RelativePoint(e))
     }
+
 }
