83ffb9b09f68c49821410cee0ed8da84388d5ff8
David Meybohm <dmeybohm@gmail.com>
2024-06-08 21:33:15 -0400

    Remove other ancillary irrelvant info

--- src/main/kotlin/com/daveme/chocolateCakePHP/Inflector.kt
+++ src/main/kotlin/com/daveme/chocolateCakePHP/Inflector.kt
@@ -6,11 +6,6 @@ package com.daveme.chocolateCakePHP
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.2.9
- * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 object Inflector {
 
