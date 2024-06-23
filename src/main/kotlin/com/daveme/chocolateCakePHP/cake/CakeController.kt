140dfea46c331a273d732e3f38191f48ba4a1bba
David Meybohm <dmeybohm@gmail.com>
2024-06-22 20:11:54 -0400

    Refactor ActionNames a little

--- src/main/kotlin/com/daveme/chocolateCakePHP/cake/CakeController.kt
+++ src/main/kotlin/com/daveme/chocolateCakePHP/cake/CakeController.kt
@@ -56,10 +56,8 @@ fun actionNameFromMethod(method: Method): ActionName {
 
 data class ActionNames(
     val defaultActionName: ActionName,
-    val allActionNames: List<ActionName>,
+    val otherActionNames: List<ActionName> = listOf(),
 ) {
-    val otherActionNames: List<ActionName>
-        get() = allActionNames.filter { it != defaultActionName }
 }
 
 /**
@@ -75,18 +73,18 @@ fun actionNamesFromControllerMethod(method: Method): ActionNames {
             as Collection<MethodReference>
 
     val defaultActionName = actionNameFromMethod(method)
-    val actionNames: List<ActionName> = renderCalls.mapNotNull {
+    val otherActionNames: List<ActionName> = renderCalls.mapNotNull {
         if (it.name != "render") {
             return@mapNotNull null
         }
         val firstParameter = it.parameterList?.getParameter(0) as? StringLiteralExpression
             ?: return@mapNotNull null
         return@mapNotNull actionNameFromPath(firstParameter.contents)
-    } + listOf(defaultActionName)
+    }
 
     return ActionNames(
         defaultActionName = defaultActionName,
-        allActionNames = actionNames
+        otherActionNames = otherActionNames
     )
 }
 
@@ -108,7 +106,7 @@ fun actionNamesFromSingleConstantRenderExpression(methodReference: MethodReferen
     val actionName = actionNameFromPath(renderParameter)
     return ActionNames(
         defaultActionName = actionName,
-        allActionNames = listOf(actionName)
+        otherActionNames = listOf()
     )
 }
 
@@ -128,7 +126,6 @@ fun viewFilenameToActionName(
             val actionName = ActionName(camelCaseActionName, "")
             return ActionNames(
                 defaultActionName = actionName,
-                allActionNames = listOf(actionName)
             )
         }
         is CakeTwoTemplatesDir -> {
@@ -137,7 +134,6 @@ fun viewFilenameToActionName(
             val actionName = ActionName(trimmed, "")
             return ActionNames(
                 defaultActionName = actionName,
-                allActionNames = listOf(actionName)
             )
         }
     }
