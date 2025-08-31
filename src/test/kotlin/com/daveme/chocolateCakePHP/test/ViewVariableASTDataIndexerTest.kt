bdfd68f9efd901869a44e28bcc8815b594715802
David Meybohm <dmeybohm@gmail.com>
2025-08-31 19:53:09 -0400

    Inprogress

--- src/test/kotlin/com/daveme/chocolateCakePHP/test/ViewVariableASTDataIndexerTest.kt
+++ src/test/kotlin/com/daveme/chocolateCakePHP/test/ViewVariableASTDataIndexerTest.kt
@@ -41,4 +41,39 @@ class ViewVariableASTDataIndexerTest : BasePlatformTestCase() {
         // with both case 1 and case 2 support
         assertTrue("AST-only implementation compiles successfully", true)
     }
+    
+    fun `test variable indirection cases compile`() {
+        val phpCode = """
+            <?php
+            namespace App\Controller;
+            
+            use Cake\Controller\Controller;
+            
+            class TestController extends Controller {
+                public function compactExample() {
+                    // Case 3: compact() function call
+                    ${'$'}this->set(compact('user', 'title'));
+                }
+                
+                public function tupleExample() {
+                    // Case 4: tuple assignment
+                    ${'$'}this->set(['name1', 'name2'], [${'$'}val1, ${'$'}val2]);
+                }
+                
+                public function variableIndirectionExample() {
+                    // Case 5 & 6: variable indirection (implemented as placeholder)
+                    ${'$'}data = ['user' => ${'$'}currentUser];
+                    ${'$'}this->set(${'$'}data);
+                }
+            }
+        """.trimIndent()
+
+        val psiFile = myFixture.configureByText("TestController.php", phpCode)
+        
+        assertNotNull("PSI file should be created", psiFile)
+        assertTrue("File should have content", psiFile.text.isNotEmpty())
+        
+        // Test that all the additional parsing cases compile successfully
+        assertTrue("All variable indirection cases compile successfully", true)
+    }
 }
