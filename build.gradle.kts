8f9596ce25cb9bd7bcc0205a54425db55a7db717
David Meybohm <dmeybohm@gmail.com>
2025-11-11 17:55:32 -0500

    Update to 2024.2.6
    - Upgrade to 2024.2.6 API version
    - Upgrade Kotlin to compatible version
    - Upgrade JVM toolchain to compatible version

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 // Configure IntelliJ Platform Gradle Plugin 2.x - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
