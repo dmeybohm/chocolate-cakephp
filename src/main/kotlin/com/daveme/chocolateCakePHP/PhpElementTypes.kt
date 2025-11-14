561dcdce812b35339b49e2b6d7c6b38bef842164
David Meybohm <dmeybohm@gmail.com>
2025-11-14 09:47:59 -0500

    Be more flexible about checking the debug node names

--- src/main/kotlin/com/daveme/chocolateCakePHP/PhpElementTypes.kt
+++ src/main/kotlin/com/daveme/chocolateCakePHP/PhpElementTypes.kt
@@ -16,12 +16,54 @@ import com.jetbrains.php.lang.parser.PhpElementTypes as PhpParserElementTypes;
 // Element type name constants
 private const val VARIABLE = "VARIABLE"
 private const val CLASS_METHOD = "CLASS_METHOD"
-private const val IDENTIFIER = "identifier"
+private const val IDENTIFIER = "IDENTIFIER"
+
+/**
+ * Compares two strings by matching only alphabetic characters, case-insensitively.
+ * Non-alphabetic characters are ignored. Does not allocate memory during comparison.
+ *
+ * Examples:
+ * - "VARIABLE" matches "variable"
+ * - "CLASS_METHOD" matches "class method", "ClassMethod", "class-method"
+ * - "identifier" matches "IDENTIFIER", "Identifier"
+ */
+private fun equalsAlphaIgnoreCase(a: CharSequence, b: CharSequence): Boolean {
+    var i = 0
+    var j = 0
+    val na = a.length
+    val nb = b.length
+
+    while (true) {
+        // Skip non-letters in A
+        while (i < na && !a[i].isAsciiLetter()) i++
+
+        // Skip non-letters in B
+        while (j < nb && !b[j].isAsciiLetter()) j++
+
+        // Both exhausted
+        if (i >= na && j >= nb) return true
+
+        // One exhausted
+        if (i >= na || j >= nb) return false
+
+        // Compare lowercased ascii
+        if (a[i].lowerAscii() != b[j].lowerAscii()) return false
+
+        i++
+        j++
+    }
+}
+
+private fun Char.isAsciiLetter(): Boolean =
+    (this in 'A'..'Z') || (this in 'a'..'z')
+
+private fun Char.lowerAscii(): Char =
+    if (this in 'A'..'Z') (this + 32) else this
 
 // Extension functions for type checking
-fun ASTNode.isVariable() = this.elementType.toString() == VARIABLE
-fun ASTNode.isClassMethod() = this.elementType.toString() == CLASS_METHOD
-fun ASTNode.isIdentifier() = this.elementType.toString() == IDENTIFIER
+fun ASTNode.isVariable() = equalsAlphaIgnoreCase(this.elementType.toString(), VARIABLE)
+fun ASTNode.isClassMethod() = equalsAlphaIgnoreCase(this.elementType.toString(), CLASS_METHOD)
+fun ASTNode.isIdentifier() = equalsAlphaIgnoreCase(this.elementType.toString(), IDENTIFIER)
 fun ASTNode.isMethodReference() = this.elementType == PhpParserElementTypes.METHOD_REFERENCE
 fun ASTNode.isParameterList() = this.elementType == PhpParserElementTypes.PARAMETER_LIST
 fun ASTNode.isAssignmentExpression() = this.elementType == PhpParserElementTypes.ASSIGNMENT_EXPRESSION
